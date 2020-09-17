35137eacd8ead96a02b2cc29c172b6eb0fd79a31
Anna Abbott <annaabbottdesign@gmail.com>
2020-09-16 20:28:51 -0600

    new index file

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,15 @@
-document.querySelector("header >h1").innerText = "Inspector Clouseau";
-document.querySelector("header > h2").innerText = "All I require is a telephone, my little bag of tools, and some privacy with which to work. That is all I require.";
+document.querySelector("header >h1").innerText = "High/Low Guessing Game";
+document.querySelector("header > h2").innerText = "Are you thinking what I'm thinking?";
 
-function windowProperties() {
-    let windowDimensions = document.querySelector("#windowSize");
-    let windowDimensionsMessage = `The browser window is ${window.innerWidth} pixels W by ${window.innerHeight} pixels H.`;
-    windowDimensions.innerText = windowDimensionsMessage;
+// computer chooses a number from 1 to 15.
+const randomNumber = Math.floor(Math.random() * 14) + 1;
+console.log(randomNumber);
 
-    let windowOffset = document.querySelector("#windowOffset");
-    let offsetMessage = `The browser offset from the top left corner of the viewport is ${window.screenX} pixels across and ${window.screenY} pixels down.`;
-    windowOffset.innerText = offsetMessage;
-};
-
-function windowSize() {
-    let windowDimensions = document.querySelector("#windowSize");
-    let windowDimensionsMessage = `The browser window is ${window.innerWidth} pixels W by ${window.innerHeight} pixels H.`;
-    windowDimensions.innerText = windowDimensionsMessage;
-
-    console.log('windowSize');
-};
-
-function windowPosition() {
-    let windowOffset = document.querySelector("#windowOffset");
-    let offsetMessage = `The browser offset from the top left corner of the viewport is ${window.screenX} pixels across and ${window.screenY} pixels down.`;
-    windowOffset.innerText = offsetMessage;
-
-    console.log('windowPosition');
-};
-
-function pageURL() {
-    let documentURL = document.querySelector("#documentURL");
-    let urlMessage = `The page URL is ${document.URL}.`;
-    documentURL.innerText = urlMessage;
+// get the user's guess. Compare to computer's number.
+function checkGuess() {
+    console.log("working button");
+    // make sure guess was in range, or ask again.
+    // print statement too low, too high, just right
 }
 
-function documentTitle() {
-    let pageTitle = document.querySelector("#documentTitle");
-    let titleMessage = `The page title is ${document.title}.`;
-    pageTitle.innerText = titleMessage;
-}
-
-function documentUpdate() {
-    let updateTime = document.querySelector("#updateTime");
-    let updateMessage = `The page was last updated on ${document.lastModified}.`;
-    updateTime.innerText = updateMessage;
-}
-
-//windowProperties();
-windowSize();
-windowPosition();
-pageURL();
-documentTitle();
-documentUpdate();
-
-window.onresize(windowSize);
-window.ondrag(windowPosition);
+// award a ribbon based on number of guesses. 
